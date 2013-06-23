2c01b19611fc0ee1fd286ce0120545aa7eabfe65
jimakous <dimitris.savoulidis@gmail.com>
2013-06-23 23:29:32 +0000

    bonus work - specific version of apache

---
+++ 2_2.pp
@@ -0,0 +1,16 @@
+  package { 'httpd-2.2.3':
+    ensure => latest,
+    before => File['/etc/httpd/conf/httpd.conf'],
+  }
+  file {'/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    mode   => 600,
+    source => '/root/examples/httpd.conf',
+  }
+
+  service { 'httpd':
+    ensure    => running ,
+    enable    => true ,
+    subscribe => File['/etc/httpd/conf/httpd.conf'],
+  }
+
